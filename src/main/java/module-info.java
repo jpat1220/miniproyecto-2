1a64bb8c514a31f57db277030b43562acaf5a949
Jorge Patiño <jorge.patino@correounivalle.edu.co>
2024-10-16 12:18:27 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.miniproyecto2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.miniproyecto2 to javafx.fxml;
+    exports com.example.miniproyecto2;
+}
